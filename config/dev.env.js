6140339ad35afe0446348b3b769eec2fbb3d058e
王强 <943446906@qq.com>
2020-06-18 19:48:35 +0800

    上传头像

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,4 +5,5 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   BASE_API: '"http://localhost/"',
+  OSS_PATH: '"https://zxktest.oss-cn-shenzhen.aliyuncs.com"'
 })
--- src/views/teacher/form.vue
+++ src/views/teacher/form.vue
@@ -21,7 +21,31 @@
         <el-input v-model="teacher.intro" :rows="10" type="textarea" />
       </el-form-item>
 
-      <!-- 讲师头像：TODO -->
+      <!-- 讲师头像 -->
+      <el-form-item label="讲师头像">
+
+        <!-- 头衔缩略图 -->
+        <pan-thumb :image="teacher.avatar" />
+        <!-- 文件上传按钮 -->
+        <el-button type="primary" icon="el-icon-upload" @click="imagecropperShow=true">更换头像
+        </el-button>
+
+        <!--
+      v-show：是否显示上传组件
+      :key：类似于id，如果一个页面多个图片上传控件，可以做区分
+      :url：后台上传的url地址
+      @close：关闭上传组件
+      @crop-upload-success：上传成功后的回调 -->
+        <image-cropper
+          v-show="imagecropperShow"
+          :width="300"
+          :height="300"
+          :key="imagecropperKey"
+          :url="BASE_API+'/admin/oss/file/upload'"
+          field="file"
+          @close="close"
+          @crop-upload-success="cropSuccess"/>
+      </el-form-item>
 
       <el-form-item>
         <el-button :disabled="saveBtnDisabled" type="primary" @click="saveOrUpdate">保存</el-button>
@@ -32,19 +56,27 @@
 
 <script>
 import teacher from '@/api/teacher'
+import ImageCropper from '@/components/ImageCropper'
+import PanThumb from '@/components/PanThumb'
+
+const defaultForm = {
+  name: '',
+  sort: 0,
+  level: 1,
+  career: '',
+  intro: '',
+  avatar: process.env.OSS_PATH + '/avatar/default.png'
+}
 
 export default {
+  components: { ImageCropper, PanThumb },
   data() {
     return {
-      teacher: {
-        name: '',
-        sort: 0,
-        level: 1,
-        career: '',
-        intro: '',
-        avatar: ''
-      },
-      saveBtnDisabled: false // 保存按钮是否禁用,
+      teacher: defaultForm,
+      saveBtnDisabled: false, // 保存按钮是否禁用
+      imagecropperShow: false, // 控制上传弹窗
+      imagecropperKey: 0, // 上传组件id
+      BASE_API: process.env.BASE_API
     }
   },
   watch: {
@@ -56,6 +88,20 @@ export default {
     this.init()
   },
   methods: {
+
+    close() {
+      this.imagecropperShow = false
+      // 上传失败后，重新打开上传组件时初始化组件，否则显示上一次的上传结果
+      this.imagecropperKey = this.imagecropperKey + 1
+    },
+
+    cropSuccess(data) {
+      console.log(data)
+      this.imagecropperShow = false
+      this.teacher.avatar = data.url
+      // 上传成功后，重新打开上传组件时初始化组件，否则显示上一次的上传结果
+      this.imagecropperKey = this.imagecropperKey + 1
+    },
 
     saveOrUpdate() {
       this.saveBtnDisabled = true
@@ -72,7 +118,7 @@ export default {
         this.fetchDataById(id)
       } else {
         // 重置表单
-        this.teacher = {}
+        this.teacher = { ...defaultForm }
       }
     },
 
